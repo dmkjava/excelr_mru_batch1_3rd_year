c167bdcd3f313dd726711c0542dc7f99d2b9fa23
dmkjava <dmk.java@gmail.com>
2025-07-12 09:30:19 +0530

    adding delete operatoin on table, menu ex

---
+++ JavaScript/ES6/higher_order_fun/ex2_filter_oddno.js
@@ -0,0 +1,10 @@
+var nos = [4, 13, 1, 6, 9, 7, 15, 17, 3, 2, 18, 11]
+// task : find only odd nos from given array. 
+
+var odd_nos = nos.filter(el=>el%2 == 1)
+// [13,1, 9, 7, 15, 17, 3, 11]
+console.log(nos);
+
+console.log(odd_nos);
+
+

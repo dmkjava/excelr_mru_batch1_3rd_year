59e1aa37a33556e6ef687b9a7174f6f7310dd306
dmkjava <dmk.java@gmail.com>
2025-07-12 12:31:29 +0530

    adding psudo classes ex, reduce fun and ex, asynchronus execution ex, todos list app

---
+++ JavaScript/ES6/ex1_set.js
@@ -0,0 +1,6 @@
+var nos = [10, 12, 10, 15, 12, 20, 5, 10, 15, 5];
+let s1 = new Set(nos);
+console.log(s1);
+
+let unk_nos= [...s1];
+console.log(unk_nos);
---
+++ JavaScript/ES6/higher_order_fun/ex3_reduce_demo.js
@@ -0,0 +1,11 @@
+const nos = [10, 20, 30, 40, ];
+// find the totals: 
+
+let total = nos.reduce((ctot, ele)=>ctot + ele);
+
+console.log(nos);
+
+console.log(total);
+
+
+
